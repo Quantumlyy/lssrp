4ab92add7752160568563e4c4a6c0e3ea652e59b
Nejc Drobnič <ethoarx@gmail.com>
2021-08-10 01:10:33 +0200

    refactor: center form

--- lssrp_app/static_src/tailwind.config.js
+++ lssrp_app/static_src/tailwind.config.js
@@ -27,7 +27,7 @@ module.exports = {
          * Adjust the following line to match your project structure.
          */
         '../../templates/**/*.html',
-        
+
         /* 
          * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
          * Adjust the following line to match your project structure.
@@ -51,7 +51,11 @@ module.exports = {
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            container: {
+                center: true
+            }
+        },
     },
     variants: {
         extend: {},
