e0d7a8e3e78ffc70c3a3ec35657ba4a011dc1575
Nejc Drobnič <ethoarx@gmail.com>
2021-08-09 23:51:42 +0200

    feat: custom simple register page

--- lssrp_app/views.py
+++ lssrp_app/views.py
@@ -1,5 +1,21 @@
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
 
 
 class HomeView(TemplateView):
     template_name = "lssrp/base.html"
+
+
+# https://dev.to/coderasha/create-advanced-user-sign-up-view-in-django-step-by-step-k9m
+def signup_view(request):
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password1")
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect("/")
+    return render(request, "lssrp/register.html", {"form": form})
--- lssrp_core/urls.py
+++ lssrp_core/urls.py
@@ -12,10 +12,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path
 
 import lssrp_app.views
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("", lssrp_app.views.HomeView.as_view(), name="home"),
+    path("register/", lssrp_app.views.signup_view, name="register"),
 ]
