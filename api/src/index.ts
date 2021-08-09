a7811494f3d0ed23bcb1fce343e5c9edca8d348a
Nejc Drobnič <ethoarx@gmail.com>
2021-08-09 22:59:15 +0200

    refactor: clear

--- api/src/index.ts
+++
@@ -1,9 +0,0 @@
-import 'reflect-metadata';
-import { port } from './config';
-import gqlServer from './server';
-
-const server = gqlServer();
-
-server.listen({ port: Number(port) }, () => {
-	console.log(`server started on http://localhost:${port}`);
-});
