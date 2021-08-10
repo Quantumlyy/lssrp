e4f5acbea9e59393a275cbf2632708f6bae05671
Nejc Drobnič <ethoarx@gmail.com>
2021-08-10 08:57:50 +0200

    fix: valid variables

--- lssrp_app/views.py
+++ lssrp_app/views.py
@@ -11,25 +11,23 @@ class HomeView(TemplateView):
 
 # https://dev.to/coderasha/create-advanced-user-sign-up-view-in-django-step-by-step-k9m
 def register_view(request):
-    form = StyledUserCreationForm(request.POST)
-    if form.is_valid():
-        form.save()
-        username = form.cleaned_data.get("username")
-        password = form.cleaned_data.get("password1")
+    register_form = StyledUserCreationForm(request.POST)
+    if register_form.is_valid():
+        register_form.save()
+        username = register_form.cleaned_data.get("username")
+        password = register_form.cleaned_data.get("password1")
         user = authenticate(username=username, password=password)
         login(request, user)
         return redirect("/")
-    return render(request, "auth/register.html", {"form": form})
+    return render(request, "auth/register.html", {"form": register_form})
 
 
 # https://stackoverflow.com/questions/31482178/django-login-page-for-non-admin-dashboard
 def login_view(request):
-    form = StyledAuthenticationForm(request.POST)
-    if form.is_valid():
-        form.save()
-        username = form.cleaned_data.get("username")
-        password = form.cleaned_data.get("password")
-        user = authenticate(username=username, password=password)
-        login(request, user)
+    login_form = StyledAuthenticationForm(request=request, data=request.POST)
+    if login_form.is_valid():
+        print("Hi")
+        login_form.clean()
+        login(request, login_form.get_user())
         return redirect("/")
-    return render(request, "auth/login.html", {"form": form})
+    return render(request, "auth/login.html", {"form": login_form})
