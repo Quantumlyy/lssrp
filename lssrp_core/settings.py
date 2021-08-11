00d211378f4efa8b0561794a4dc072edc44c37bd
Nejc Drobnič <ethoarx@gmail.com>
2021-08-11 06:52:03 +0200

    refactor: use correct timezone

--- lssrp_core/settings.py
+++ lssrp_core/settings.py
@@ -121,7 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = "sl-SI"
-TIME_ZONE = "UTC"
+TIME_ZONE = "CET"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
