ca084091047627e29eee61168960d391c83509a4
Nejc Drobnič <ethoarx@gmail.com>
2021-08-20 06:11:18 +0200

    feat: render stringified email

--- lssrp_app/views.py
+++ lssrp_app/views.py
@@ -19,6 +19,8 @@ class MailView(TemplateView):
         context = super().get_context_data(**kwargs)
 
         context["profile"] = models.MailProfile(user=self.request.user)
+        context["received"] = models.Email.objects.all().filter(receiver=context["profile"].user_id)
+        context["sent"] = models.Email.objects.all().filter(sender=context["profile"].user_id)
 
         return context
 
